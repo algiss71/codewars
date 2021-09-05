6b0202044cdc23cc36146bbf51a18d6381388f78
Sergey <gemodialez71@gmail.com>
2021-09-05 19:44:03 +0300

    edit strsum

--- strsum/main.c
+++ strsum/main.c
@@ -1,28 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-
 #include <malloc.h>
 #include <string.h>
-char * utoa_builtin_div(int value, char *buffer)
-{
-   buffer += 11;
-// 11 байт достаточно для десятичного представления 32-х байтного числа
-// и  завершающего нуля
-   *--buffer = 0;
-   do
-   {
-      *--buffer = value % 10 + '0';
-      value /= 10;
-   }
-   while (value != 0);
-   return buffer;
-}
-/*char *strsum(const char *a, const char *b)
+
+char *strsum(const char *a, const char *b)
 {
     return NULL;
-}*/
+}
 int main()
 {
     char s[]="one";
